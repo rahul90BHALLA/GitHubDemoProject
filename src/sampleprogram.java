18d15e90b5d61ab51dc07ddbf252fa04b118a49e
hp <hp@RSA>
2021-02-02 13:24:54 +0530

    commit

---
+++ src/sampleprogram.java
@@ -0,0 +1,20 @@
+
+public class sampleprogram {
+
+	void m1(){
+		System.out.println("skjvskj");
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		sampleprogram bb =  new sampleprogram();
+		bb.m1();
+		
+	}
+
+}
